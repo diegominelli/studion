2b0615fa8bca9ef0f789278298aa527434d588da
Diego Minelli <diego.minelli@hotmail.com>
2021-04-21 19:27:08 -0300

    Conhecendo o SMTP.js

--- src/script.js
+++ src/script.js
@@ -8,4 +8,23 @@ $(document).ready(() => {
       '<button type="button" class="slick-next slider-next-arrow">Next</button>',
   };
   $(".slider").slick(slickOptions);
+
+  $(".footer-form-button").on("click", () => {
+    const email = $("#email").val();
+    Email.send({
+      Host: "smtp.yourisp.com",
+      Username: "username",
+      Password: "password",
+      To: "them@website.com",
+      From: email,
+      Subject: "Pir favor me adicione na newslatter",
+      Body: `Olá,
+
+      Eu gostaria de ser adicionado na newslatter do site.
+      Meu email é ${email}.
+      
+      Obrigado!
+      `,
+    }).then((message) => alert(message));
+  });
 });
